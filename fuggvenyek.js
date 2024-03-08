21f749feb6da8c78709f5bb26a90ebbf5f55b794
PalkAlex23 <alexpytmusic@gmail.com>
2024-03-08 11:36:18 +0100

    kesz javascript

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,3 +1,8 @@
 export function htmlOsszeAllit(lista) {
-   
+   /* Itt állítjuk össze a html kódot */
+   let txt = ""
+   for (let i = 0; i < lista.length; i++) {
+        txt += `<div class="kep"><img src="${lista[i].kep}" alt="${lista[i].cim}"></div>`
+   }
+   return txt
 }
--- main.js
+++ main.js
@@ -3,21 +3,28 @@ import { htmlOsszeAllit } from "./fuggvenyek.js";
 
 /*1. feladat:  galéria kisképeinek elhelyezése */
 /* fogd meg a .galeria elemet */
-
+const GALERIA = document.querySelector(".galeria")
 /* írd bele az összeálíltott html képsorozatot */
-
+GALERIA.innerHTML = htmlOsszeAllit(KEPEK)
 
 /*2. feladat  A .nagykep divben lévő img elem megfogása */
-
+const nagykepELEMEK = document.querySelectorAll(".nagykep img")
 /* A .nagykep div megfogása */
-
+const nagykepDIV = document.querySelector(".nagykep")
 
 
 /*3. feladat-  eseménykezelő hozzáadása a kisképekhez  */
 /* Az előzőekben létrehozott kisképek megfogása (querySelectorAll) - ez egy lista lesz */
-
+const galeriaELEMEK = document.querySelectorAll(".galeria img")
 /* eseménykezelő hozzáadása a kisképekhez, ciklussal */
+let index = 0
 
+for (let i = 0; i < galeriaELEMEK.length; i++) {
+    galeriaELEMEK[i].addEventListener("click", function() {
+        index = i
+        nagykepELEMEK[0].src = galeriaELEMEK[i].src
+    })
+}
 
 
 
@@ -25,5 +32,21 @@ import { htmlOsszeAllit } from "./fuggvenyek.js";
 
 
 
-
 /* 4. feladat .bal és a .jobb gombokra kattintva léptessük a galériát */
+const BAL = document.querySelector(".bal")
+BAL.addEventListener("click", function() {
+    index -= 1
+    if (index < 0) {
+        index = galeriaELEMEK.length-1
+    }
+    nagykepELEMEK[0].src = galeriaELEMEK[index].src
+})
+
+const JOBB = document.querySelector(".jobb")
+JOBB.addEventListener("click", function() {
+    index += 1
+    if (index >= galeriaELEMEK.length) {
+        index = 0
+    }
+    nagykepELEMEK[0].src = galeriaELEMEK[index].src
+})
